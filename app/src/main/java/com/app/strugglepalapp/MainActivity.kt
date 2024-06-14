104fc16f2afa5004561468bd264899535cafe539
Alex <alexandre.chastaing@non.se.com>
2024-06-14 16:55:18 +0200

    poc: try loading one image

--- app/src/main/java/com/app/strugglepalapp/MainActivity.kt
+++ app/src/main/java/com/app/strugglepalapp/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.app.strugglepalapp.ui.theme.StrugglePalAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,10 +37,20 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier.weight(1f)
+        )
+        AsyncImage(
+            model = ImageRequest.Builder(context = LocalContext.current)
+                .data("https://en.wikipedia.org/wiki/Simba#/media/File:Simba_(_Disney_character_-_adult).png")
+                .crossfade(true)
+                .build(),
+            contentDescription = "example",
+            modifier = Modifier.weight(1f)
+        )
+    }
 }
 
 @Preview(showBackground = true)
